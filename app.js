24c7795491a1eeda06d0e0ce10fe541452329e84
Irfan Ainuddin <irfandin@gmail.com>
2018-03-28 12:57:06 -0700

    /route:id basics

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict';
 
 var express = require('express');
+var app = express();
+
+app.use(function(req, res, next){
+  console.log("first middleware piece");
+  next();
+});
+
+// : denotes express paramater
+app.use('/different:id',function(req, res, next){
+  console.log("second middleware piece" + req.params.id);
+  next();
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log("Express server is listening on port", port)
+});
